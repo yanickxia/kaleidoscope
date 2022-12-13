d60abde6d4306f9c79dc7efe3b1b8142c5edcf4e
yanick <me.yan.xia@qq.com>
2022-12-13 17:55:21 +0800

    update cmake

---
+++ parser/parser.cpp
@@ -0,0 +1,5 @@
+//
+// Created by yanick on 2022/12/13.
+//
+
+#include "parser.h"
---
+++ parser/parser.h
@@ -0,0 +1,8 @@
+//
+// Created by yanick on 2022/12/13.
+//
+
+#ifndef KALEIDOSCOPE_PARSER_H
+#define KALEIDOSCOPE_PARSER_H
+
+#endif //KALEIDOSCOPE_PARSER_H
